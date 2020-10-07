9e7142f79006dc21e3850b12049a3b8dfbd80d58
Eddie Reeder <e.reeder1997@gmail.com>
2020-10-07 07:49:27 +0100

    Replaced link to facebook with link to SO

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -40,18 +40,19 @@ const Badge: React.FC = () => {
           </svg>
         </a>
         <a
-          href="https://www.facebook.com/eddi.reeder"
+          href="https://stackoverflow.com/users/9127393/eddie-reeder?tab=profile"
           target="_blank"
           rel="noopener noreferrer"
           className="social-button neu-card"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+            width="36"
+            height="36"
+            viewBox="0 0 120 120"
           >
-            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+            <path d="M84.4 93.8V70.6h7.7v30.9H22.6V70.6h7.7v23.2z" />
+            <path d="M38.8 68.4l37.8 7.9 1.6-7.6-37.8-7.9-1.6 7.6zm5-18l35 16.3 3.2-7-35-16.4-3.2 7.1zm9.7-17.2l29.7 24.7 4.9-5.9-29.7-24.7-4.9 5.9zm19.2-18.3l-6.2 4.6 23 31 6.2-4.6-23-31zM38 86h38.6v-7.7H38V86z" />
           </svg>
         </a>
       </div>
