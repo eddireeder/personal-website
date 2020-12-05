b79b469ce31f77b77c3f1fe63d492c63b98c7488
Eddie Reeder <e.reeder1997@gmail.com>
2020-12-05 10:51:38 +0000

    Added experience block for Waters and Alelion

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Experience from "./Experience/Experience";
 import "./Experiences.scss";
 import watersLogo from "../../assets/images/waters-logo.png";
+import alelionLogo from "../../assets/images/alelion-logo.png";
 
 const Experiences: React.FC = () => {
   return (
@@ -13,9 +14,25 @@ const Experiences: React.FC = () => {
         role="Software Engineer"
         company="Waters"
         from="Dec 2019"
-        to="Jan 2020"
-        bullets={["Bullet 1.", "Bullet 2."]}
+        to="Present"
+        bullets={[
+          "Developed the frontend for a breakthrough COVID-19 diagnosis machine.",
+          "Worked with the AME team in daily sprints and activities.",
+          "Learnt ASP.NET and to write robust automated tests in Gherkin.",
+        ]}
         image={watersLogo}
+      />
+      <Experience
+        role="Software Engineer"
+        company="Alelion"
+        from="Jul 2017"
+        to="Aug 2018"
+        bullets={[
+          "Developed the frontend for a breakthrough COVID-19 diagnosis machine.",
+          "Worked with the AME team in daily sprints and activities.",
+          "Learnt ASP.NET and to write robust automated tests in Gherkin.",
+        ]}
+        image={alelionLogo}
       />
     </div>
   );
