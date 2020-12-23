9d7d3429de133f1ed037e614e0f37bf549eed513
Eddie Reeder <e.reeder1997@gmail.com>
2020-12-23 11:28:14 +0000

    Remove all old project files

--- src/components/BulletPoint/BulletPoint.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./BulletPoint.scss";
-
-const BulletPoint: React.FC = () => {
-  return <div className="BulletPoint" />;
-};
-
-export default BulletPoint;
--- src/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer.scss";
-
-const Footer: React.FC = () => {
-  return <div className="Footer"></div>;
-};
-
-export default Footer;
